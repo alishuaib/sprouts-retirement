a4301e1809eddbe97a6e775ff20fc7e427cc94ca
Qelvah <mysmae.lite@outlook.com>
2026-09-12 16:26:55 -0400

    fix the pseudo farm

--- scripts/farm.gd
+++ scripts/farm.gd
@@ -12,18 +12,30 @@ var sprites :Dictionary = {
 		"time": 60},
 }
 
+enum GROWTH_STAGES {
+	EMPTY,
+	GROWING,
+	MATURED
+}
+
 func _ready():
 	# Adds the marker2d into an array to represent the tiles
 	var markers = get_node("MarkerList")
 	for row in markers.get_children():
 		for coord:Marker2D in row.get_children():
-			tiles[coord.position] = {"rect": Rect2(coord.position, tile_size), "timer": null, "produce_Key": null, "stage": null, "sprite": null}
+			tiles[coord.position] = {"rect": Rect2(adjust_rect_position(coord.position), tile_size), "timer": null, "produce_Key": null, "stage": GROWTH_STAGES.EMPTY, "sprite": null}
 
+# because Rect2 position is top left and not middle of itself
+func adjust_rect_position(coord :Vector2):
+	return Vector2(coord.x-(tile_size.x/2), coord.y-(tile_size.y/2))
+	
 # get a tile's center coordinate
+# there has to be a better way than looping with for right?
 func get_tile(mouse_position: Vector2):
 	for coord in tiles:
 		if tiles[coord]["rect"].has_point(mouse_position):
 			return coord
+	
 
 func _input(_event :InputEvent):
 	if Input.is_action_just_released("Tile Select"):
@@ -33,7 +45,7 @@ func _input(_event :InputEvent):
 			return
 		
 		# If click is within a tile, creates a timer 
-		if tiles[tile_coord]["stage"] == null: 
+		if tiles[tile_coord]["stage"] == GROWTH_STAGES.EMPTY: 
 			var newTimer: Timer = Timer.new()
 			newTimer.wait_time = sprites["tomato"]["time"]/20
 			newTimer.one_shot = true
@@ -43,9 +55,9 @@ func _input(_event :InputEvent):
 			tiles[tile_coord]["produce_key"] = "tomato"
 			process_tile(tile_coord)
 			$TimerList.add_child(newTimer)
-		elif tiles[tile_coord]["stage"] == 1:
+		elif tiles[tile_coord]["stage"] == GROWTH_STAGES.GROWING:
 			print("in progress...")
-		elif tiles[tile_coord]["stage"] == 2:
+		elif tiles[tile_coord]["stage"] == GROWTH_STAGES.MATURED:
 			process_tile(tile_coord)
 	
 func timer_done(tile_coord: Vector2):
@@ -55,35 +67,43 @@ func timer_done(tile_coord: Vector2):
 		process_tile(tile_coord)
 
 func process_tile(tile_coord: Vector2):
-	var stage = tiles[tile_coord]["stage"]
+	var tile :Dictionary = tiles[tile_coord]
 	
-	if stage == null:
-		tiles[tile_coord]["stage"] = 1
-		print("tile has reached stage 1 and is growing")
-	if stage == 1:
-		tiles[tile_coord]["stage"] = 2
-		print("tile has reached stage 2 and is collectable")
-	if stage == 2:
-		tiles[tile_coord]["stage"] = null
-		print("tile has been collected and is now null")
-	process_sprite(tile_coord, tiles[tile_coord]["stage"])
+	match tile["stage"]:
+		GROWTH_STAGES.EMPTY:
+			tile["stage"] = GROWTH_STAGES.GROWING
+			print("tile has reached stage 1 and is growing")
+		GROWTH_STAGES.GROWING:
+			tile["stage"] = GROWTH_STAGES.MATURED
+			print("tile has reached stage 2 and is collectable")
+		GROWTH_STAGES.MATURED:
+			tile["stage"] = GROWTH_STAGES.EMPTY
+			print("tile has been collected and is now empty")
+	
+	# process tile based on new stage
+	process_sprite(tile_coord)
 
-func process_sprite(tile_coord :Vector2, stage):
+func process_sprite(tile_coord :Vector2):
+	var tile :Dictionary = tiles[tile_coord]
 	
-	if (tiles[tile_coord]["sprite"] != null):
-			tiles[tile_coord]["sprite"].queue_free()
-			tiles[tile_coord]["sprite"] = null
-		
-	if stage != null: 
+	# if new stage is empty and the sprite isn't removed, remove the sprite
+	if tile["stage"] == GROWTH_STAGES.EMPTY:
+		if tile["sprite"] != null:
+			tile["sprite"].queue_free()
+			tile["sprite"] = null
+		return null
+	
+	# if the sprite is null, create a new sprite
+	if tile["sprite"] == null:
 		var newSprite = Sprite2D.new()
-		if stage == 1: 
-			newSprite.texture = sprites[tiles[tile_coord]["produce_key"]]["stage1"]
-		elif stage == 2:
-			newSprite.texture = sprites[tiles[tile_coord]["produce_key"]]["stage2"]
+		newSprite.scale = Vector2(0.5, 0.5) # using a 32x32 during tests
 		newSprite.position = tile_coord
 		$TileFilled.add_child(newSprite)
 		tiles[tile_coord]["sprite"] = newSprite
 	
-	print("tile_coord ", tile_coord)
-	print("click_coord ", get_local_mouse_position())
-	if tiles[tile_coord]["sprite"]: print("sprite_coord" , tiles[tile_coord]["sprite"].position)
+	# adjust sprite based on stage
+	match tile["stage"]:
+		GROWTH_STAGES.GROWING:
+			tile["sprite"].texture = sprites[tiles[tile_coord]["produce_key"]]["stage1"]
+		GROWTH_STAGES.MATURED:
+			tile["sprite"].texture = sprites[tiles[tile_coord]["produce_key"]]["stage2"]
